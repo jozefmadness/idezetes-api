4aa0f1d4cbe1513cfeb0c0524ddfd5446096310d
beresjozsef <b.jozsef@gmail.com>
2016-08-11 11:39:53 +0200

    3

--- app/Http/api_routes.php
+++ app/Http/api_routes.php
@@ -12,12 +12,13 @@ $api->version('v1', function ($api) {
 	$api->group(['middleware' =>['api.auth']], function ($api) {
 		$api->post('user/addquoted', 'App\Api\V1\Controllers\QuoteController@AddQuoted');
 		$api->post('user/addquote', 'App\Api\V1\Controllers\QuoteController@AddQuote');
-		$api->post('user/qetqoutename', 'App\Api\V1\Controllers\QuoteController@getQbyname');
+		$api->post('user/qetquotename', 'App\Api\V1\Controllers\QuoteController@getQbyname');
 		$api->post('user/getquotecategory', 'App\Api\V1\Controllers\QuoteController@getQbycategory');
 		$api->get('user/getquoted','App\Api\V1\Controllers\QuoteController@getQuoted');
 		$api->get('user/getallquote','App\Api\V1\Controllers\QuoteController@getAllQuote');
 		$api->get('user/getallquoted','App\Api\V1\Controllers\QuoteController@getAllQuoted');
 		$api->get('user/randomquote','App\Api\V1\Controllers\QuoteController@randomquote');
+		$api->post('user/getbysource','App\Api\V1\Controllers\QuoteController@getBySource');
 	});
 	// example of protected route
 	$api->get('protected', ['middleware' => ['api.auth'], function () {		
